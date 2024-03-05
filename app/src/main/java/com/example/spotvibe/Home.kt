74edf1850840e8a4b0b08fa397b151eee0c0a933
lopezcg <carlosgablopezm@gmail.com>
2024-03-04 23:03:46 -0500

    Redireccionamiento a Busqueda desde home

--- app/src/main/java/com/example/spotvibe/Home.kt
+++ app/src/main/java/com/example/spotvibe/Home.kt
@@ -1,7 +1,10 @@
 package com.example.spotvibe
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.spotvibe.adapter.Eventadapter
@@ -11,7 +14,13 @@ class Home : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
         initRecyclerView()
+        val intent= Intent(this, Busqueda::class.java)
+        val imageButton=findViewById<ImageView>(R.id.imageView2)
+        imageButton.setOnClickListener{
+            startActivity(intent)
+        }
     }
+
     fun initRecyclerView(){
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerEvent)
         recyclerView.layoutManager= LinearLayoutManager(this)
