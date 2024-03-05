1d1652c6f8a8d1596cc4fe2ca8ed490b283dee99
LondoFeli <felilondoo@outlook.es>
2024-03-05 11:13:47 -0500

    Duenio crea evento pantalla

---
+++ app/src/main/java/com/example/spotvibe/DuenioCreaEvento.kt
@@ -0,0 +1,11 @@
+package com.example.spotvibe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DuenioCreaEvento : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_duenio_crea_evento)
+    }
+}
