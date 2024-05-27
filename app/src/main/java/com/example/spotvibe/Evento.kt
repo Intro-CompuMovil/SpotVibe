d589fb299471350b4e4d3f89e053fcb999707867
LondoFeli <felilondoo@outlook.es>
2024-05-27 13:01:18 -0500

    MErge arreglado

--- app/src/main/java/com/example/spotvibe/Evento.kt
+++ app/src/main/java/com/example/spotvibe/Evento.kt
@@ -2,7 +2,6 @@ package com.example.spotvibe
 
 data class Evento(
     var id: String? = null,
-    var id: String = "",
     val autor: String = "",
     val cantidadInscritos: Int = 0,
     val cantidadParticipantes: String = "",
